93bc4ab0ee80f2e7134eb8a147ed594a08a34c7b
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-08-12 00:35:25 -0600

    code cleanup

--- frontend/src/components/InputField/inputField.styles.js
+++ frontend/src/components/InputField/inputField.styles.js
@@ -3,14 +3,15 @@ import styled from "styled-components";
 export const MainWrapper = styled.div`
   > span {
     color: white;
+    margin-bottom: 1rem;
   }
 `;
 
 export const InputWrapper = styled.input`
-  width: 100%;
-  margin: 0 auto 1rem auto;
-  border-radius: 5px;
-  height: 2.5rem;
+  margin-bottom: 1rem;
+  /* width: 100%; */
+  /* border-radius: 5px;
+  height: 2.5rem; */
 `;
 
 export const ErrorMessageWrapper = styled.div`
