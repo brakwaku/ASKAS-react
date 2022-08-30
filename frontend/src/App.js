595a2c5f97ed3f6ebe7c4096d28edc82646f0122
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-08-29 21:11:08 -0600

    add signup route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import HomeScreen from './screens/Home/Home';
 import LoginScreen from './screens/Login/Login';
+import SignupScreen from './screens/signup/Signup';
 import Footer from './components/Footer/Footer';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Header />}>
           <Route index element={<HomeScreen />} />
           <Route path='login' element={<LoginScreen />} />
+          <Route path='signup' element={<SignupScreen />} />
         </Route>
       </Routes>
       <Footer />
